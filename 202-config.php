4c9b586565ce819263325bd0af0e1f213ded20f4
cachebit <417209993@qq.com>
2017-06-12 14:01:26 +0000

    initial commit

---
+++ 202-config.php
@@ -0,0 +1,8 @@
+<?php
+
+// ** MySQL settings ** //
+$dbname = 'homestead';    		// The name of the database
+$dbuser = 'homestead';     		  		// Your MySQL username
+$dbpass = 'homestead'; 			// ...and password
+$dbhost = 'localhost';    					// 99% chance you won't need to change this value
+$mchost = 'localhost';  					//this is the memcache server host, if you don't know what this is, don't touch it.
